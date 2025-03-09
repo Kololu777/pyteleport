16da627703421affce9fa10259e4d094650791b9
ko <kosiynon31kos@gmail.com>
2025-03-10 00:20:10 +0900

    add: .gitignore case

--- src/pyteleport/core/tree.py
+++ src/pyteleport/core/tree.py
@@ -1,154 +1,130 @@
-import os
-from rich import print
 import fnmatch
+import os
+
+from rich import print
+
 
 def tree(
-        path:str, 
-        is_hidden_file:bool = False, 
-        _prefix:str="", 
-        _visited_list:list[str] | None = None,
-        _visited_dict:dict[str, str] | None = None
-    ):
+    path: str,
+    is_hidden_file: bool = False,
+    _prefix: str = "",
+    _visited_list: list[str] | None = None,
+):
     """
     Get tree structure of the path. like `tree` command.
 
     Args:
         path: start path
         is_hidden_file: if True, hidden files are included.
-        verbose: if True, print tree structure.
     Returns:
         list: tree structure of the path.
-    
+
     Examples:
         >>> from pyteleport import tree
         >>> tree("./src", is_hidden_file=True)
     """
-    # Init visited list and dict
+    # Init visited list.
     if _visited_list is None:
         _visited_list = []
-    if _visited_dict is None:
-        _visited_dict = {}
-        
-    if _prefix == "":
-       # add root info to visited list and dict
-       _visited_dict[path] = {}
-       _visited_dict[path]["symbol"] = ""
-       _visited_dict[path]["name"] = path
-       _visited_dict[path]["path"] = path
-       _visited_dict[path]["number"] = len(_visited_list)
-       _visited_dict[path]["is_dir"] = os.path.isdir(path) # directory
-       
-       _visited_list.append(path)
 
-       if not _visited_dict[path]["is_dir"]: # If init path is file, return
-            return _visited_list, _visited_dict
+    if _prefix == "":
+        # add root info to visited list ad dict
+        visited_dict = {
+            "symbol": "",
+            "name": path,
+            "path": path,
+            "is_dir": os.path.isdir(path),
+        }
+        _visited_list.append(visited_dict)
+
+        if not _visited_list[0]["is_dir"]:  # If init path is file, return
+            return _visited_list
     entries = sorted(os.listdir(path))
 
     if not is_hidden_file:
-        entries = [entry for entry in entries if not entry.startswith('.')] # exclude hidden files.
+        entries = [
+            entry for entry in entries if not entry.startswith(".")
+        ]  # exclude hidden files.
     entries_count = len(entries)
-    
+
     for idx, entry in enumerate(entries):
         full_path = os.path.join(path, entry)
         connector = "└── " if idx == entries_count - 1 else "├── "
-        if os.path.isdir(full_path):
-            _visited_dict[f"{_prefix}{connector}{entry}"] = {}
-            _visited_dict[f"{_prefix}{connector}{entry}"]["symbol"] = f"{_prefix}{connector}"
-            _visited_dict[f"{_prefix}{connector}{entry}"]["name"] = entry
-            _visited_dict[f"{_prefix}{connector}{entry}"]["path"] = full_path
-            _visited_dict[f"{_prefix}{connector}{entry}"]["number"] = len(_visited_list)
-            _visited_dict[f"{_prefix}{connector}{entry}"]["is_dir"] = True # directory
-            _visited_list.append(f"{_prefix}{connector}{entry}")
-
-        else:
-            _visited_dict[f"{_prefix}{connector}{entry}"] = {}
-            _visited_dict[f"{_prefix}{connector}{entry}"]["symbol"] = f"{_prefix}{connector}"
-            _visited_dict[f"{_prefix}{connector}{entry}"]["name"] = entry
-            _visited_dict[f"{_prefix}{connector}{entry}"]["path"] = full_path
-            _visited_dict[f"{_prefix}{connector}{entry}"]["number"] = len(_visited_list)
-            _visited_dict[f"{_prefix}{connector}{entry}"]["is_dir"] = False # file
-            _visited_list.append(f"{_prefix}{connector}{entry}")
+        visited_dict = {
+            "symbol": f"{_prefix}{connector}",
+            "name": entry,
+            "path": full_path,
+            "is_dir": os.path.isdir(full_path),
+        }
+        _visited_list.append(visited_dict)
 
         if os.path.isdir(full_path):
             next_prefix = _prefix + ("    " if idx == entries_count - 1 else "│   ")
-            tree(full_path, is_hidden_file, next_prefix, _visited_list, _visited_dict)
-    return _visited_list, _visited_dict
+            tree(full_path, is_hidden_file, next_prefix, _visited_list)
+    return _visited_list
+
 
 class Tree:
-    def __init__(self, path:str, is_hidden_file:bool = False):
+    def __init__(self, path: str, is_hidden_file: bool = False):
         self.path = path
         self.is_hidden_file = is_hidden_file
-        self._tree_list, self._tree_dict = tree(path, is_hidden_file)
+        self._tree_list = tree(path, is_hidden_file)
 
     @property
-    def tree_list(self)->list[str]:
+    def tree_list(self) -> list[str]:
         return self._tree_list
 
     @property
-    def tree_dict(self)->dict[str, str]:
-        return self._tree_dict
+    def __len__(self) -> int:
+        return len(self._tree_list)
 
     @property
-    def print(self)->None:
-        for k in self._tree_list:
-            if self._tree_dict[k]["is_dir"]:
-                print(f"{self._tree_dict[k]['symbol']}[magenta]{self._tree_dict[k]['name']}[/magenta]")
+    def print(self) -> None:
+        for tree_dict in self._tree_list:
+            if tree_dict["is_dir"]:
+                print(f"{tree_dict['symbol']}[magenta]{tree_dict['name']}[/magenta]")
             else:
-                print(f"{self._tree_dict[k]['symbol']}[cyan]{self._tree_dict[k]['name']}[/cyan]")
+                print(f"{tree_dict['symbol']}[cyan]{tree_dict['name']}[/cyan]")
 
-    def change_name_root(self, change_root_name:str)->None:
+    def change_name_root(self, change_root_name: str) -> None:
         # {"src": {"path":"./", "number":1}} -> {"tests": {"path":"./tests", "number":1}}
         # Before Data
-        self._update_tree(self._tree_list[0], change_root_name, mode="replace")
-  
+        self._update_tree(0, change_root_name, mode="replace")
 
     def all_change_name_leaf(
-            self, 
-            change_leef_name:str, 
-            include:list[str] = [".*"],
-            exclude:list[str] | None = None,
-            is_dir:bool = True
-        )->None:
+        self,
+        change_leef_name: str,
+        include: list[str] = [".*"],
+        exclude: list[str] | None = None,
+        is_if_dir_change: bool = True,
+    ) -> None:
         if len(self._tree_list) <= 1:
             return
-        for _, key in enumerate(self._tree_list[1:]):
-            file_name = self._tree_dict[key]["path"]
+        for idx, tree_dict in enumerate(self._tree_list):
+            file_name = tree_dict["path"]
             if exclude is not None:
                 if any(fnmatch.fnmatch(file_name, ext) for ext in exclude):
                     continue
-            if include is not None:
-                if any(fnmatch.fnmatch(file_name, file) for file in include):
-                    self._update_tree(key, update_name=change_leef_name, mode="add")
-                elif is_dir and self._tree_dict[key]["is_dir"]:
-                    self._update_tree(key, update_name=change_leef_name, mode="add")
-                else:
-                    continue
-            
+            if any(fnmatch.fnmatch(file_name, file) for file in include):
+                self._update_tree(idx, update_name=change_leef_name, mode="add")
+            elif is_if_dir_change and tree_dict["is_dir"]:
+                self._update_tree(idx, update_name=change_leef_name, mode="add")
+            else:
+                continue
 
-            
-    def _update_tree(self, key:str, update_name:str, mode:str="add")->None:
+    def _update_tree(self, idx: int, update_name: str, mode: str = "add") -> None:
         if mode == "add":
-            new_name = update_name + self._tree_dict[key]["name"]
-            new_leef_name = self._tree_dict[key]["symbol"] + new_name
+            update_name = update_name + self._tree_list[idx]["name"]
         elif mode == "replace":
-            new_name = update_name
-            new_leef_name = self._tree_dict[key]["symbol"] + new_name
+            update_name = update_name
         else:
             raise ValueError(f"Invalid mode: {mode}")
+        self._tree_list[idx]["name"] = update_name
+        self._tree_list[idx]["path"] = self._tree_list[idx]["path"] + update_name
 
-        idx = self._tree_dict[key]["number"]
-
-        # update tree_list
-        self._tree_list[idx] = new_leef_name
-
-        # update tree_dict
-        self._tree_dict[new_leef_name] = {}
-        self._tree_dict[new_leef_name]["symbol"] = self._tree_dict[key]["symbol"]
-        self._tree_dict[new_leef_name]["name"] = new_name
-        self._tree_dict[new_leef_name]["path"] = self._tree_dict[key]["path"]
-        self._tree_dict[new_leef_name]["number"] = self._tree_dict[key]["number"]
-        self._tree_dict[new_leef_name]["is_dir"] = self._tree_dict[key]["is_dir"]
-        self._tree_dict.pop(key)
-        
-        
-        
+    def exclude_leaf(self, exclude: list[str] | None = None) -> None:
+        if exclude is None:
+            return
+        for idx, tree_dict in enumerate(self._tree_list):
+            if any(fnmatch.fnmatch(tree_dict["path"], file) for file in exclude):
+                self._tree_list.pop(idx)
--- src/pyteleport/core/tree_parse.py
+++ src/pyteleport/core/tree_parse.py
@@ -1,9 +1,9 @@
 from pyteleport import Tree
 from pyteleport.integration import read_gitignore
+import fnmatch
 if __name__ == "__main__":
     a = Tree("./src")
     print(a.tree_list)
-    print(a.tree_dict)
 
     tree_change = Tree("./src")
   
@@ -12,17 +12,19 @@ if __name__ == "__main__":
     print(include_list)
     print(exclude_list)
 
-    print("--------------------------------")
-    for k in tree_change.tree_list:
-        print(k)
-    print("--------------------------------")
+    tree_change.print
+
+    
+    tree_change.change_name_root("test")
     tree_change.all_change_name_leaf(
         change_leef_name="test_",
         include=include_list,
         exclude=exclude_list,
-        is_dir=True
+        is_if_dir_change=True,
     )
-    for k in tree_change.tree_list:
-        print(k)
-        #print(tree_change.tree_dict[k])
-    
+    print("--------------------------------")
+
+    tree_change.print
+
+    a = "./src/pyteleport.egg-info/PKG-INFO"
+    print(fnmatch.fnmatch(a, "*.egg-info"))
